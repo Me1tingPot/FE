97e7af3e3b95dec97ac301863a1ccc351780b7d7
김용민 <dydals3440@gmail.com>
2024-05-18 18:10:06 +0900

    feat: useModal 훅

--- src/hooks/queries/useAuth.ts
+++ src/hooks/queries/useAuth.ts
@@ -6,3 +6,4 @@ import { useMutation, useQuery } from '@tanstack/react-query';
 //       ...mutationOptions,
 //     });
 //   }
+
--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -15,3 +15,5 @@ function useModal() {
 }
 
 export default useModal;
+
+
