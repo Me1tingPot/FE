6195b200e39eca24076a0695b91a4212a0f07e65
chaKK <cdin23@naver.com>
2024-05-26 12:55:29 +0900

    Style: SignUp location component UI

--- src/components/signup/Location.tsx
+++ src/components/signup/Location.tsx
@@ -1,22 +1,35 @@
+import { useState } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { colors } from '@/constants';
 import useThemeStore from '@/store/useThemeStore';
 import { ThemeMode } from '@/types';
 import CustomButton from '../common/CustomButton';
+import CustomTextInput from '../common/CustomTextInput';
 
 type LocationProps = {
 	onNext: () => void;
 };
 
 const Location = ({ onNext }: LocationProps) => {
+	const {
+		control,
+		formState: { errors },
+		setValue,
+	} = useFormContext();
+	// TODO | locaion 입력 받는 형식, 검색 방식 확인 후 UI 및 로직 수정
+	const [location, setLocation] = useState('');
+	const [city, setCity] = useState('');
+	const [town, setTown] = useState('');
 	const { theme } = useThemeStore();
 	const styles = styling(theme);
 	const { t } = useTranslation();
 
 	return (
 		<View style={styles.container}>
-			<ScrollView>
+			<ScrollView contentContainerStyle={styles.contentsContainer}>
 				<View>
 					<Text style={styles.title}>{t('어디에 계신가요?')}</Text>
 					<Text style={styles.description}>
@@ -24,6 +37,63 @@ const Location = ({ onNext }: LocationProps) => {
 						<Text style={styles.textPoint}>{t('가까운 파티')}</Text>
 						{t('를 추천해드릴게요.')}
 					</Text>
+				</View>
+
+				<Controller
+					control={control}
+					name="location"
+					render={({ field: { onChange, onBlur, value } }) => (
+						<Pressable onPress={() => console.log('click')}>
+							<CustomTextInput
+								value={location ? location : '위치 선택'}
+								onChangeText={onChange}
+								onBlur={onBlur}
+								variant={'success'}
+								icon={
+									<MaterialIcons
+										name="keyboard-arrow-down"
+										color={colors[theme].GRAY_300}
+										size={25}
+									/>
+								}
+								editable={false}
+							/>
+						</Pressable>
+					)}
+				/>
+
+				<View style={styles.smallDropDownContainer}>
+					<Pressable onPress={() => console.log('click')} style={{ flex: 1 }}>
+						<CustomTextInput
+							value={city ? city : '시/도'}
+							onChangeText={() => {}}
+							variant={'default'}
+							icon={
+								<MaterialIcons
+									name="keyboard-arrow-down"
+									color={colors[theme].GRAY_300}
+									size={25}
+								/>
+							}
+							editable={false}
+						/>
+					</Pressable>
+
+					<Pressable onPress={() => console.log('click')} style={{ flex: 1 }}>
+						<CustomTextInput
+							value={town ? town : '시/군/구'}
+							onChangeText={() => {}}
+							variant={'default'}
+							icon={
+								<MaterialIcons
+									name="keyboard-arrow-down"
+									color={colors[theme].GRAY_300}
+									size={25}
+								/>
+							}
+							editable={false}
+						/>
+					</Pressable>
 				</View>
 			</ScrollView>
 
@@ -48,6 +118,11 @@ const styling = (theme: ThemeMode) =>
 			paddingVertical: 50,
 			paddingHorizontal: 40,
 		},
+		contentsContainer: {
+			display: 'flex',
+			flexDirection: 'column',
+			rowGap: 20,
+		},
 		buttonPosition: {
 			marginTop: 'auto',
 		},
@@ -63,6 +138,11 @@ const styling = (theme: ThemeMode) =>
 		textPoint: {
 			color: colors[theme].GRAY_700,
 			fontWeight: '700',
+		},
+		smallDropDownContainer: {
+			display: 'flex',
+			flexDirection: 'row',
+			gap: 20,
 		},
 	});
 
