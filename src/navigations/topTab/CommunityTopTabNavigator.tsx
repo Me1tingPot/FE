63cc584c56cdcfe531bb59fc014af682ac6592d1
chaKK <cdin23@naver.com>
2024-05-29 17:07:34 +0900

    Style: CommunityTopTabNavigator tabBar style 수정

--- src/navigations/topTab/CommunityTopTabNavigator.tsx
+++ src/navigations/topTab/CommunityTopTabNavigator.tsx
@@ -22,10 +22,9 @@ function CommunityTopTabNavigator() {
 				screenOptions={{
 					tabBarLabelStyle: {
 						fontSize: 20,
-						fontFamily: 'Pretendard-Bold',
+						fontFamily: 'Pretendard-Light',
 					},
 					tabBarInactiveTintColor: colors[theme].GRAY_500,
-					tabBarActiveTintColor: colors[theme].PINK_700,
 					tabBarStyle: {
 						backgroundColor: colors[theme].WHITE,
 					},
