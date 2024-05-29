5321bb45f42e04c3be1cc4987991799202b981da
김용민 <dydals3440@gmail.com>
2024-05-30 02:09:47 +0900

    Revert "Feature/#10 WishList 페이지 구현, Compound Card 구현 Bottom Button 구현 "

--- src/constants/navigations.ts
+++ src/constants/navigations.ts
@@ -25,16 +25,9 @@ const communityNavigations = {
 	COMMUNITY_POSTING_DETAIL: 'CommunityPostingDetail',
 } as const;
 
-const myNavigations = {
-	MY_HOME: 'MyHome',
-	EDIT_PROFILE: 'EditProfile',
-	MY_WRITE_POST: 'MyWritePost',
-} as const;
-
 export {
 	authNavigations,
 	feedTabNavigations,
 	wishNavigations,
 	communityNavigations,
-	myNavigations,
 };
--- src/navigations/tab/FeedTabNavigator.tsx
+++ src/navigations/tab/FeedTabNavigator.tsx
@@ -6,11 +6,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { RouteProp } from '@react-navigation/native';
 import FeedHomeHeaderLeft from '@/components/feed/FeedHomeHeaderLeft';
 import FeedHomeScreen from '@/screens/feed/FeedHomeScreen';
+import MyHomeScreen from '@/screens/my/MyHomeScreen';
 import PartyHomeScreen from '@/screens/party/PartyHomeScreen';
 import useThemeStore from '@/store/useThemeStore';
 import { colors, feedTabNavigations } from '../../constants';
-import MyStackNavigator from '../stack/MyStackNavigator';
-import CommunityTopTabNavigator from '../topTab/CommunityTopTabNavigator';
 import CommunityStackNavigator from '../stack/CommunityStackNavigator';
 import WishTopTabNavigator from '../topTab/WishTopTabNavigator';
 
@@ -122,7 +121,7 @@ function FeedTabNavigator() {
 			/>
 			<Tab.Screen
 				name={feedTabNavigations.MY_HOME}
-				component={MyStackNavigator}
+				component={MyHomeScreen}
 				options={({ navigation }) => ({
 					tabBarLabel: `${t('마이페이지')}`,
 				})}
