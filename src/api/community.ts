5eda529e2fbe07b21aa99c64a3d836c78215a151
daindaind <cdin23@naver.com>
2024-07-06 17:51:22 +0900

    Feat: Posting Post API 연결

--- src/api/community.ts
+++ src/api/community.ts
@@ -21,7 +21,6 @@ export interface GetPostsProps {
 }
 
 const post = async ({title, content, postType, imageKeys}: PostProps) => {
-   console.log({title, content, postType, imageKeys})
    const { data } = await axiosInstance.post(`${API_URL.POST}`, {
       title,
       content,
