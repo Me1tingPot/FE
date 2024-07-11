e6242e0a16b0c9a9c54dcf0b99e373bb9ed81ad5
daindaind <cdin23@naver.com>
2024-07-11 17:28:32 +0900

    Feat: 파티 작성 UI 연결

--- src/hooks/useGetAddress.ts
+++ src/hooks/useGetAddress.ts
@@ -9,10 +9,13 @@ function useGetAddress(location: LatLng) {
 	const [address, setAddress] = useState('');
 
 	useEffect(() => {
+		const source = axios.CancelToken.source();
+
 		(async () => {
 			try {
 				const { data } = await axios.get(
 					`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&result_type=street_address|route|political&key=${Config.GOOGLE_MAPS_API_KEY}&language=ko`,
+					{ cancelToken: source.token }, // CancelToken을 요청에 추가합니다.
 				);
 
 				const address = data.results.length
@@ -21,9 +24,18 @@ function useGetAddress(location: LatLng) {
 
 				setAddress(address);
 			} catch (error) {
-				setAddress(errorMessages.CANNOT_GET_ADDRESS);
+				if (axios.isCancel(error)) {
+					console.log('Request canceled', error.message);
+				} else {
+					console.log(error);
+					setAddress(errorMessages.CANNOT_GET_ADDRESS);
+				}
 			}
 		})();
+
+		return () => {
+			source.cancel('Operation canceled by the user.');
+		};
 	}, [latitude, longitude]);
 
 	return address;
--- src/screens/party/PartyWriteScreen.tsx
+++ src/screens/party/PartyWriteScreen.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import {
+	KeyboardAvoidingView,
+	Platform,
+	SafeAreaView,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import CustomButton from '@/components/common/CustomButton';
 import CustomTextInput from '@/components/common/CustomTextInput';
 import ImageInput from '@/components/common/ImageInput';
 import PreviewImageList from '@/components/common/PreviewImageList';
+import SelectTrueOrNot from '@/components/party/SelectTrueOrNot';
 import DatePickerOption from '@/components/signup/DatePickerOption';
 import { colors, partyNavigations } from '@/constants';
 import useGetAddress from '@/hooks/useGetAddress';
@@ -14,7 +23,7 @@ import usePermission from '@/hooks/usePermission';
 import { PartyStackParamList } from '@/navigations/stack/PartyStackNavigator';
 import useThemeStore from '@/store/useThemeStore';
 import { ThemeMode } from '@/types';
-import { getDateWithSeparator } from '@/utils';
+import { getDateWithSeparator, getFormattedTime } from '@/utils';
 
 type AddPostScreenProps = StackScreenProps<
 	PartyStackParamList,
@@ -33,50 +42,152 @@ function PartyWriteScreen({ route }: AddPostScreenProps) {
 	const address = useGetAddress(location);
 
 	const [date, setDate] = useState(new Date());
+	const [time, setTime] = useState(new Date());
+	const [isBookedPlace, setIsBookedPlace] = useState(false);
+	const [isChangePlace, setIsChangePlace] = useState(false);
 
 	const [isDatePicked, setIsDatePicked] = useState(false);
+	const [isTimePicked, setIsTimePicked] = useState(false);
 	const dateModal = useModal();
+	const timeModal = useModal();
+	const selectBookedPlaceTrueOrNot = useModal();
+	const selectChangePlaceTrueOrNot = useModal();
 
 	const handleConfirmDate = () => {
 		setIsDatePicked(true);
 		dateModal.hide();
 	};
 
+	const handleConfirmTime = () => {
+		setIsTimePicked(true);
+		timeModal.hide();
+	};
+
 	const handleChangeDate = (pickedDate: Date) => {
 		setDate(pickedDate);
 	};
 
+	const handleChangeTime = (pickedTime: Date) => {
+		console.log(pickedTime);
+		setTime(pickedTime);
+	};
+
 	return (
-		<SafeAreaView style={styles.container}>
-			<ScrollView contentContainerStyle={styles.contentContainer}>
-				<ImageInput onChange={imagePicker.handleChange} size="medium" />
-				<PreviewImageList
-					imageUris={imagePicker.imageUris}
-					onDelete={imagePicker.delete}
-					onChangeOrder={imagePicker.changeOrder}
-				/>
-				<View style={styles.inputContainer}>
-					<CustomTextInput value={address} onChangeText={() => {}} />
-					<CustomTextInput value="" onChangeText={() => {}} />
-					<CustomTextInput value="" onChangeText={() => {}} />
-					<CustomButton
-						variant="outlined"
-						size="large"
-						label={
-							isDatePicked ? getDateWithSeparator(date, '. ') : '날짜 선택'
-						}
-						onPress={dateModal.show}
+		<KeyboardAvoidingView
+			behavior="padding"
+			keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 70}
+			style={styles.container}
+		>
+			<SafeAreaView style={styles.container}>
+				<ScrollView contentContainerStyle={styles.contentContainer}>
+					<ImageInput onChange={imagePicker.handleChange} size="medium" />
+					<PreviewImageList
+						imageUris={imagePicker.imageUris}
+						onDelete={imagePicker.delete}
+						onChangeOrder={imagePicker.changeOrder}
 					/>
-				</View>
-				<DatePickerOption
-					date={date}
-					isVisible={dateModal.isVisible}
-					onChangeDate={handleChangeDate}
-					onConfirmDate={handleConfirmDate}
-					hideOption={dateModal.hide}
-				/>
-			</ScrollView>
-		</SafeAreaView>
+					<View style={styles.inputContainer}>
+						<Text style={styles.inputTitle}>
+							파티 제목<Text style={styles.privateInput}>*</Text>
+						</Text>
+						<CustomTextInput value="" onChangeText={() => {}} />
+						<Text style={styles.inputTitle}>
+							파티 장소<Text style={styles.privateInput}>*</Text>
+						</Text>
+						<CustomTextInput value={address} onChangeText={() => {}} />
+						<CustomTextInput
+							value=""
+							onChangeText={() => {}}
+							placeholder="상세 주소를 입력해주세요"
+						/>
+						<Text style={styles.inputTitle}>
+							최소 인원<Text style={styles.privateInput}>*</Text>
+						</Text>
+						<CustomTextInput
+							inputMode="numeric"
+							value=""
+							onChangeText={() => {}}
+						/>
+						<Text style={styles.inputTitle}>
+							최대 인원<Text style={styles.privateInput}>*</Text>
+						</Text>
+						<CustomTextInput
+							inputMode="numeric"
+							value=""
+							onChangeText={() => {}}
+						/>
+						<Text style={styles.inputTitle}>
+							파티 날짜<Text style={styles.privateInput}>*</Text>
+						</Text>
+						<CustomButton
+							variant="outlined"
+							size="large"
+							label={
+								isDatePicked ? getDateWithSeparator(date, '. ') : '날짜 선택'
+							}
+							onPress={dateModal.show}
+						/>
+						<Text style={styles.inputTitle}>
+							시작 시간<Text style={styles.privateInput}>*</Text>
+						</Text>
+						<CustomButton
+							variant="outlined"
+							size="large"
+							label={
+								isTimePicked && time ? getFormattedTime(time) : '시간 선택'
+							}
+							onPress={timeModal.show}
+						/>
+						<Text style={styles.inputTitle}>
+							장소 대관 여부<Text style={styles.privateInput}>*</Text>
+						</Text>
+						<CustomButton
+							variant="outlined"
+							size="large"
+							label={isBookedPlace ? '예' : '아니오'}
+							onPress={selectBookedPlaceTrueOrNot.show}
+						/>
+						<Text style={styles.inputTitle}>
+							장소 변경 가능 여부<Text style={styles.privateInput}>*</Text>
+						</Text>
+						<CustomButton
+							variant="outlined"
+							size="large"
+							label={isChangePlace ? '예' : '아니오'}
+							onPress={selectChangePlaceTrueOrNot.show}
+						/>
+						<Text style={styles.inputTitle}>상세 설명</Text>
+						<CustomTextInput multiline value="" onChangeText={() => {}} />
+					</View>
+					<DatePickerOption
+						mode="date"
+						date={date}
+						isVisible={dateModal.isVisible}
+						onChangeDate={handleChangeDate}
+						onConfirmDate={handleConfirmDate}
+						hideOption={dateModal.hide}
+					/>
+					<DatePickerOption
+						mode="time"
+						date={time}
+						isVisible={timeModal.isVisible}
+						onChangeDate={handleChangeTime}
+						onConfirmDate={handleConfirmTime}
+						hideOption={timeModal.hide}
+					/>
+					<SelectTrueOrNot
+						isVisible={selectBookedPlaceTrueOrNot.isVisible}
+						hideOption={selectBookedPlaceTrueOrNot.hide}
+						setter={setIsBookedPlace}
+					/>
+					<SelectTrueOrNot
+						isVisible={selectChangePlaceTrueOrNot.isVisible}
+						hideOption={selectChangePlaceTrueOrNot.hide}
+						setter={setIsChangePlace}
+					/>
+				</ScrollView>
+			</SafeAreaView>
+		</KeyboardAvoidingView>
 	);
 }
 
@@ -91,7 +202,15 @@ const styling = (theme: ThemeMode) =>
 		},
 		inputContainer: {
 			gap: 20,
-			marginBottom: 20,
+			marginVertical: 20,
+		},
+		inputTitle: {
+			fontSize: 18,
+			fontFamily: 'Pretendard-Light',
+		},
+		privateInput: {
+			fontSize: 18,
+			color: colors[theme].RED_500,
 		},
 	});
 
